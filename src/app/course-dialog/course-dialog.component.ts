a063221fd93c48d5b3d3ec7ff513cd2cfbbe4eeb
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-06-20 21:38:18 +0530

    concatMap completed

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -40,14 +40,40 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
     ngOnInit() {
       // So for continous saving and drafting of values
       // from the form. The form provides an observable
-      this.form.valueChanges.subscribe(val => {
-        console.log(val);
+      this.form.valueChanges
+      .pipe(
+        filter(() => this.form.valid),
+        concatMap(changes =>
+          this.saveCourse(changes)
+        )
+        // concatMap is taking up all the changes in the form
+        // converting them into an observable
+        // subscribing to it
+        // and at the end concating it the original observable
+      )
+      .subscribe(changes => {
+        // fromPromise is an inbuilt rxjs method that takes a promise and creates an rxjs observable
+
+        // snince now concatMap() has come hence we dont require below statements
+        // const saveCourse$ = this.saveCourse(changes);
+
+        // saveCourse$.subscribe();
       });
       // valueChanges observable is a good example of stream of values
-      
 
 
 
+
+    }
+
+    saveCourse(changes) {
+      return fromPromise(fetch(`/api/courses/${this.course.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(changes),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }));
     }
 
 
